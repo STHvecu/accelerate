84eb01d5b9a4222069be9d9b3b2ccbebfc166ec7
STHvecu <leealbertilorenzo@gmail.com>
2017-11-14 15:52:29 -0500

    Addition of Featured Work in homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -23,6 +23,48 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+
+  <section class="featured-work">
+        <h4>Featured Work</h4>
+    
+    <div class="site-content">  
+      
+      <?php $args = array (
+            'posts_per_page' => 3,
+            'post_type' => 'case_studies',
+            'orderby' => 'title',
+            'order' => 'ASC'
+      ); ?>
+      <?php query_posts($args); ?>
+    
+      <div class="homepage-featured-work">
+    
+          <?php while (have_posts()) : the_post(); 
+              $image_1 = get_field("image_1");
+              $size = "medium";
+          ?>
+          <div class="individual-featured-work">
+            <figure>
+              <a href="<?php the_permalink(); ?>">  
+                <?php echo wp_get_attachment_image($image_1, $size); ?> </a>
+            </figure>
+            <h6>
+              <a href="<?php the_permalink(); ?>">   
+                <?php the_title(); ?> </a>
+            </h6>
+          </div>
+      
+          <?php endwhile; ?>
+        
+      </div>
+         
+      <?php wp_reset_query();   //resets the altered query back to the original ?>
+      
+    </div>    
+    
+  </section>
+
+
   <section class="recent-posts">
     <div class="site-content">
       <div class="blog-post">
@@ -36,5 +78,6 @@ get_header(); ?>
       </div>
     </div>
   </section>
+
 
 <?php get_footer(); ?>
